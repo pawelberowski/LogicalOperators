37f66a61876df2648a79c73b77bdf7dd021fcca3
Taz <pawelberowski@gmail.com>
2023-05-05 17:41:26 +0200

    Solved compare within margin

--- exercises.js
+++ exercises.js
@@ -7,4 +7,21 @@ console.log(shouldSetAlarm(true, true)) // false
 console.log(shouldSetAlarm(false,true)) // false
 console.log(shouldSetAlarm(true, false)) // true
 
+console.log('~~~~~~~~~~~~~~ Compare within margin ~~~~~~~~~~~~~~')
+
+function areValuesWithinMargin(valueA, valueB, margin = 0){
+    const distance = Math.abs(valueA - valueB);
+    if (distance <= margin) {
+        return 0;
+    }
+    if (valueA < valueB) {
+        return -1;
+    }
+    return 1;
+}
+console.log(areValuesWithinMargin(4, 5)) // -1
+console.log(areValuesWithinMargin(2, 5, 3)) // 0
+console.log(areValuesWithinMargin(8.1, 5, 3)) // 1
+
+
 console.log('~~~~~~~~~~~~~~ The end ~~~~~~~~~~~~~~')
