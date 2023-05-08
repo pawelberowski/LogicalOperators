01caeb781052f4d686e3307fbf30b1052ff16ed9
Taz <pawelberowski@gmail.com>
2023-05-08 14:59:44 +0200

    Solved Boolean from scratch kata

--- exercises.js
+++ exercises.js
@@ -23,5 +23,30 @@ console.log(areValuesWithinMargin(4, 5)) // -1
 console.log(areValuesWithinMargin(2, 5, 3)) // 0
 console.log(areValuesWithinMargin(8.1, 5, 3)) // 1
 
+console.log('~~~~~~~~~~~~~~ Boolean logic from scratch ~~~~~~~~~~~~~~')
+
+const isAnyValueTrue = (valueA, ValueB) => {
+    if (valueA) {
+        return true;
+    }
+    return Boolean(ValueB);
+}
+
+const isOnlyOneValueTrue = (valueA, valueB) => {
+    if (valueA) {
+        return !valueB;
+    }
+    if (valueB) {
+        return !valueA;
+    }
+    return false;
+}
+
+console.log(isAnyValueTrue(false,-66)) // true
+console.log(isAnyValueTrue(0,'')) // false
+console.log(isAnyValueTrue(true,false)) // true
+console.log(isOnlyOneValueTrue(8,'16')) // false
+console.log(isOnlyOneValueTrue(0,true)) // true
+console.log(isOnlyOneValueTrue('',false)) // false
 
 console.log('~~~~~~~~~~~~~~ The end ~~~~~~~~~~~~~~')
